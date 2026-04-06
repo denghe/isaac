0fce4b8fd181ca10a1968cf6142d66e606c586d4
denghe <denghe@hotmail.com>
2026-04-06 15:58:11 +0800

    add some desc

--- src/t3_sceneitem_exploder.cpp
+++ src/t3_sceneitem_exploder.cpp
@@ -18,32 +18,46 @@ namespace Test3 {
 
 	void Exploder::Update() {
 		XX_BEGIN(_1);
+		// 爆炸第一阶段，体积短时间内变大
+		// 计算需要多少帧，每帧的半径增量
 		numSteps = (int32_t)std::ceilf(cExplodeDuration * gg.cFps);
 		inc = (cExplodeRadius - radius) / numSteps;
+		// 开始执行体积增大循环逻辑
 		for (; numSteps > 0; --numSteps) {
 			// 爆炸检测，级联引爆
 			auto cri = scene->phys->PosToCRIndex(pos);
-			scene->phys->ForeachByRange(cri.y, cri.x, radius + 64.f, gg.sgrdd, [&](PhysSystem::Node& o, float range)->void {
+			scene->phys->ForeachByRange(cri.y, cri.x, radius + 64.f, gg.sgrdd
+				, [&](PhysSystem::Node& o, float range)->void {
+				// 过滤目标类型：炸药桶
 				if (o.value->typeId == Bucket::cTypeId) {
+					// 圆心距离判断
 					auto d = o.cache.pos - pos;
 					auto mag2 = d.x * d.x + d.y * d.y;
 					auto r = o.cache.radius + radius;
 					auto rr = r * r;
+					// 相交
 					if (mag2 < rr) {
+						// 令桶爆炸
 						((Bucket*)o.value)->Explode();
 					}
 				}
 			});
 			XX_YIELD(_1);
+			// 体积逐渐变大
 			radius += inc;
 			scale = radius * 2.f / gg.pics.cell_bucket.uvRect.w;
 		}
+		// 第二阶段，颜色淡出
+		// 计算需要多少帧，每帧的 alpha 增量
 		numSteps = (int32_t)std::ceilf(cFadeoutDuration * gg.cFps);
 		inc = 1.f / numSteps;
+		// 开始执行颜色淡出循环逻辑
 		for (; numSteps > 0; --numSteps) {
 			XX_YIELD(_1);
+			// alpha 逐渐变小
 			alpha -= inc;
 		}
+		// 自杀
 		Dispose();
 		XX_END(_1);
 	}
